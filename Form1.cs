446fbaa41dc454ebaea302d131290c57bd6e44f9
endurovojta173 <endurovojta173@DESKTOP-R8TLHAN>
2022-10-16 16:54:18 +0200

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SouboryBin04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open,FileAccess.ReadWrite);
+                BinaryReader br = new BinaryReader(fs);
+
+            }
+        }
+    }
+}
